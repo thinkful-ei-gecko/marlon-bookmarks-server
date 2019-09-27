dcec8e4c61f21859139dbf2c80d5e6140249f259
Marlon Agno <mdj.agno@gmail.com>
2019-09-27 11:41:53 -0400

    authorization step

--- src/app.js
+++ src/app.js
@@ -6,16 +6,43 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
-
+const winston = require('winston');
 const app = express();
-
 const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
   : 'common';
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.File({ filename: 'info.log' })
+  ]
+});
+
+
+if (NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.simple()
+  }));
+}
 
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
+app.use(express.json());
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
+  console.log(process.env);
+  console.log(apiToken);
+  console.log(authToken);
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    logger.error(`Unauthorized request to path: ${req.path}`);
+    return res.status(401).json({ error: 'Unauthorized request' });
+  }
+  // move to the next middleware
+  next();
+});
 
 app.get('/', (req, res) => { 
   res.send('Hello, world!');
